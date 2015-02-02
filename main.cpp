d8054247c693dbf0cc7acc78866ae4dc0ac9a93e
Porraryd <pontusorraryd@gmail.com>
2015-02-02 16:37:17 +0100

    Triangle added

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+#ifdef WIN32
+#define GLEW_STATIC
+#endif
+
 #include <GL/glew.h> // include GLEW and new version of GL on Windows
 #include <GLFW/glfw3.h> // GLFW helper library
 #include <stdio.h>
@@ -37,6 +41,37 @@ int main () {
   // tell GL to only draw onto a pixel if the shape is closer to the viewer
   glEnable (GL_DEPTH_TEST); // enable depth-testing
   glDepthFunc (GL_LESS); // depth-testing interprets a smaller value as "closer"
+
+  float points[] = {
+	  0.0f, 0.5f, 0.0f,
+	  0.5f, -0.5f, 0.0f,
+	  -0.5f, -0.5f, 0.0f
+  };
+
+  GLuint vbo = 0;
+  glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points, GL_STATIC_DRAW);
+
+  GLuint vao = 0;
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
+  glEnableVertexAttribArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+  while (!glfwWindowShouldClose(window)) {
+	  // wipe the drawing surface clear
+	  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	  //glUseProgram(shader_programme);
+	  glBindVertexArray(vao);
+	  // draw points 0-3 from the currently bound VAO with current in-use shader
+	  glDrawArrays(GL_TRIANGLES, 0, 3);
+	  // update other events like input handling 
+	  glfwPollEvents();
+	  // put the stuff we've been drawing onto the display
+	  glfwSwapBuffers(window);
+  } 
 
   // close GL context and any other GLFW resources
   glfwTerminate();
