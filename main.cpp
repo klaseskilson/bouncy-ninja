32e32e7cf43c17563e1140ec0750ef27afa88efa
Klas Eskilson <klas.eskilson@gmail.com>
2015-02-02 16:23:08 +0100

    Add GLEW and more extensive initialization program

--- main.cpp
+++ main.cpp
@@ -1,42 +1,44 @@
-#include <GLFW/glfw3.h>
+#include <GL/glew.h> // include GLEW and new version of GL on Windows
+#include <GLFW/glfw3.h> // GLFW helper library
+#include <stdio.h>
 
-int main(void)
-{
-    GLFWwindow* window;
+int main () {
+  // start GL context and O/S window using the GLFW helper library
+  if (!glfwInit ()) {
+    fprintf (stderr, "ERROR: could not start GLFW3\n");
+    return -1;
+  }
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+  #ifndef __APPLE__
+    glfwWindowHint (GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint (GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint (GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  #endif
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
-
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
-
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glColor3f(1.0f, 0.3f, 0.5f);
-        glBegin(GL_TRIANGLES); 
-        glVertex3f(0.5f, -0.2f, 0.0f);
-        glVertex3f(-0.5f, -0.2f, 0.0f);
-        glVertex3f(0.0f, 0.8f, 0.0f);
-        glEnd();
-
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
-
+  GLFWwindow* window = glfwCreateWindow (640, 480, "Hello Triangle", NULL, NULL);
+  if (!window) {
+    fprintf (stderr, "ERROR: could not open window with GLFW3\n");
     glfwTerminate();
-    return 0;
+    return 1;
+  }
+  glfwMakeContextCurrent (window);
+
+  // start GLEW extension handler
+  glewExperimental = GL_TRUE;
+  glewInit ();
+
+  // get version info
+  const GLubyte* renderer = glGetString (GL_RENDERER); // get renderer string
+  const GLubyte* version = glGetString (GL_VERSION); // version as a string
+  printf ("Renderer: %s\n", renderer);
+  printf ("OpenGL version supported %s\n", version);
+
+  // tell GL to only draw onto a pixel if the shape is closer to the viewer
+  glEnable (GL_DEPTH_TEST); // enable depth-testing
+  glDepthFunc (GL_LESS); // depth-testing interprets a smaller value as "closer"
+
+  // close GL context and any other GLFW resources
+  glfwTerminate();
+  return 0;
 }
