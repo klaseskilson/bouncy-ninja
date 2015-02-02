da88d531aecf82724f29c8f6f7c0d5ecfa3a628e
Klas Eskilson <klas.eskilson@gmail.com>
2015-02-02 16:41:03 +0100

    Add nice messages and format code prettier

--- main.cpp
+++ main.cpp
@@ -1,42 +1,53 @@
 #include <GL/glew.h> // include GLEW and new version of GL on Windows
+#include <glm/glm.hpp>
 #include <GLFW/glfw3.h> // GLFW helper library
 #include <stdio.h>
 
-int main () {
+int main()
+{
+  printf("Initiating...\n");
   // start GL context and O/S window using the GLFW helper library
-  if (!glfwInit ()) {
+  if (!glfwInit())
+  {
     fprintf (stderr, "ERROR: could not start GLFW3\n");
     return -1;
   }
 
   #ifndef __APPLE__
-    glfwWindowHint (GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint (GLFW_CONTEXT_VERSION_MINOR, 2);
-    glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    glfwWindowHint (GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   #endif
 
-  GLFWwindow* window = glfwCreateWindow (640, 480, "Hello Triangle", NULL, NULL);
-  if (!window) {
+  GLFWwindow* window = glfwCreateWindow(640, 480, "Hello Triangle", NULL, NULL);
+  if (!window)
+  {
     fprintf (stderr, "ERROR: could not open window with GLFW3\n");
     glfwTerminate();
     return 1;
   }
-  glfwMakeContextCurrent (window);
+  glfwMakeContextCurrent(window);
 
   // start GLEW extension handler
   glewExperimental = GL_TRUE;
   glewInit ();
 
   // get version info
-  const GLubyte* renderer = glGetString (GL_RENDERER); // get renderer string
-  const GLubyte* version = glGetString (GL_VERSION); // version as a string
-  printf ("Renderer: %s\n", renderer);
-  printf ("OpenGL version supported %s\n", version);
+  const GLubyte* renderer = glGetString(GL_RENDERER); // get renderer string
+  const GLubyte* version = glGetString(GL_VERSION); // version as a string
+  printf("Renderer: %s\n", renderer);
+  printf("OpenGL version supported %s\n", version);
 
   // tell GL to only draw onto a pixel if the shape is closer to the viewer
-  glEnable (GL_DEPTH_TEST); // enable depth-testing
-  glDepthFunc (GL_LESS); // depth-testing interprets a smaller value as "closer"
+  glEnable(GL_DEPTH_TEST); // enable depth-testing
+  glDepthFunc(GL_LESS); // depth-testing interprets a smaller value as "closer"
+
+  printf("\nLet's get ready to render!\n\n");
+  while (!glfwWindowShouldClose(window))
+  {
+
+  }
 
   // close GL context and any other GLFW resources
   glfwTerminate();
