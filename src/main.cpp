7abd6f366917fdc9fc52cb25e03fda914dc0bf21
Porraryd <pontusorraryd@gmail.com>
2015-02-17 15:45:09 +0100

    camera bugfix

--- src/main.cpp
+++ src/main.cpp
@@ -162,7 +162,7 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
     }
 }
 
-static void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
+void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
 {
     mouseXPos = xpos;
     mouseYPos = ypos;
