6c6ee76e29f637c2bbbc9d7574020756ee0f508a
Porraryd <pontusorraryd@gmail.com>
2015-02-17 08:15:19 +0100

    Added support for OBJ-files

--- src/Body.cpp
+++ src/Body.cpp
@@ -1,8 +1,13 @@
 #include "Body.h"
+#include <iostream>
 bool Body::mDebug = true;
+GLShader*  Body::basicShader;
 
 Body::Body()
 {
+    loadObj("../assets/suzanne.obj");
+    std::cout << vertices.at(1);
+
     Mass* tmpMass = new Mass(glm::vec3(-1.0f,1.0f,1.0f));
     Mass* tmpMass2 = new Mass(glm::vec3(1.0f,1.0f,1.0f));
     Mass* tmpMass3 = new Mass(glm::vec3(1.0f, 1.0f, -1.0f));
@@ -76,6 +81,22 @@ void Body::draw()
             (*it)->draw();
         }
     }
+    else
+    {
+        // Update the transformation
+        glm::mat4 mTransform = glm::mat4(1.0f);
+
+        glUseProgram(Mass::getShader()->programID);
+
+        // Send our transformation to the currently bound shader
+        GLuint transformID = glGetUniformLocation(Mass::getShader()->programID, "M");
+        glUniformMatrix4fv(transformID, 1, GL_FALSE, &mTransform[0][0]);
+
+        glBindVertexArray(vao);
+        // (mode, vertex count, type, element array buffer offset)
+        glDrawElements(GL_TRIANGLES, indices.size() , GL_UNSIGNED_SHORT, (void*)0);
+        glBindVertexArray(0);
+    }
 }
 
 void Body::update(float timeDelta)
@@ -89,4 +110,76 @@ void Body::update(float timeDelta)
 void Body::addBoundary(std::shared_ptr<Boundary> b)
 {
     mBoundaries.push_back(b);
+}
+
+void Body::setShader(GLShader* shader)
+{
+    basicShader = shader;
+}
+
+GLShader* Body::getShader()
+{
+    return basicShader;
+}
+
+void Body::loadObj(const char * path)
+{
+    std::vector<glm::vec3> out_vertices;
+    std::vector<glm::vec2> out_uvs;
+    std::vector<glm::vec3> out_normals;
+    
+    loadOBJ(path, out_vertices, out_uvs, out_normals);
+
+    indexVBO(out_vertices, out_uvs, out_normals, indices, vertices, uvs, normals);
+    
+    
+    for (int i = 0; i < vertices.size(); i++)
+    {
+        vertexarray.push_back(vertices.at(i).x);
+        vertexarray.push_back(vertices.at(i).y);
+        vertexarray.push_back(vertices.at(i).z);
+        vertexarray.push_back(normals.at(i).x);
+        vertexarray.push_back(normals.at(i).y);
+        vertexarray.push_back(normals.at(i).z);
+        vertexarray.push_back(uvs.at(i).x);
+        vertexarray.push_back(uvs.at(i).y);
+    }
+
+    numberOfTriangles = indices.size() / 3;
+    numberOfVertices = vertices.size();
+
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    glGenBuffers(1, &vertexbuffer);
+    glGenBuffers(1, &indexbuffer);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+
+    glBufferData(GL_ARRAY_BUFFER, 8 * vertices.size() * sizeof(GLfloat), &(vertexarray[0]), GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+    glEnableVertexAttribArray(2);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE,
+        8 * sizeof(GLfloat), (void*)0); // xyz coordinates
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE,
+        8 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat))); // normals
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE,
+        8 * sizeof(GLfloat), (void*)(6 * sizeof(GLfloat))); // texcoords
+
+    // Activate the index buffer
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexbuffer);
+    // Present our vertex indices to OpenGL
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+        indices.size() * sizeof(unsigned short), &(indices[0]), GL_STATIC_DRAW);
+
+    // Deactivate (unbind) the VAO and the buffers again.
+    // Do NOT unbind the buffers while the VAO is still bound.
+    // The index buffer is an essential part of the VAO state.
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
 }
